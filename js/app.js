2c3dd60c0207333b8550b6fa6bbf05987514dff1
Aidar <sharipovaydar200515@gmail.com>
2024-02-11 21:32:35 +0600

    asdf

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
-book_titles = ["Мать учения", "Повелитель Тайн", "Противостаяние Святого", "Воля Вечная"]
-
 const app = Vue.createApp({
     data() {
         return{
+            books: [
+                {title: "Мать учения", author: "Nobody103", img_path: "../img/MoL.jpg", fav: true},
+                {title: "Повелитель Тайн", author: "Cuttlefish that loves diving", img_path: "../img/LOTM.jpeg", fav: true },
+                {title: "Противостаяние Святого", author: "Er Gen", img_path: "../img/Renegade Immortal.jpeg", fav: true},
+                {title: "Воля Вечная", author: "Er Gen", img_path: "../img/Will Eternal.jpeg", fav: true}],
             title: "Мать учения",
             current: "0" 
         }
@@ -11,23 +14,23 @@ const app = Vue.createApp({
     methods: {
         next() {
             // Значение current на одно больше чем обычно. Не с нуля начинается
-            if (book_titles[this.current+1]==undefined){
+            if (this.books[this.current+1]==undefined){
                 this.current = 0
             } else{
                 this.current++
             }
-            this.title = book_titles[this.current]
+            this.title = this.books[this.current].title
         },
 
         previous() {
             // Значение current на одно больше чем обычно. Не с нуля начинается
-            if (book_titles[this.current-1]==undefined){
-                this.current = book_titles.length-1
+            if (this.books[this.current-1]==undefined){
+                this.current = books.length-1
             } else{
                 this.current--
             }
             console.log(this.current)
-            this.title = book_titles[this.current]
+            this.title = this.books[this.current].title
         },
 
         main_page(){
