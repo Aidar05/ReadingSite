3f778041636e42d07c58223bc94b863879255b95
Aidar <sharipovaydar200515@gmail.com>
2024-02-13 16:33:16 +0600

    Весь фронт перевел на Vue.js, для каждого компо- нента отдельный css файл

--- VueMigration/course_work/src/App.vue
+++ VueMigration/course_work/src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <Header />
+
   <Intro/> 
+  
+  <About/>
+
+  <Footer/>
 </template>
 
 <script>
+import '@/assets/css/global.css';
 import Header from './components/Header.vue';
 import Intro from './components/Intro.vue'
-import '@/assets/css/global.css';
+import About from './components/about_component/About.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
 
   components: {
-    Header, Intro
+    Header, Intro, About, Footer
   }
 }
 </script>
---
+++ VueMigration/course_work/src/components/about_component/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- Про сайт -->
+  <div class="main_container" id="about_container">
+    <div class="about_subcontainer" id="about">      
+      <Attributes/>
+
+      <Description/>
+
+      <Best_writers/>
+    </div>
+  </div>
+</template>
+
+<script>
+import "../../assets/css/about/about.css"
+import Attributes from "./Attributes.vue";
+import Description from "./Description.vue";
+import Best_writers from "./Best_writers.vue"
+
+export default {
+    name: 'About',
+    components: { 
+      Attributes, Description, Best_writers
+    }
+}
+
+</script>
