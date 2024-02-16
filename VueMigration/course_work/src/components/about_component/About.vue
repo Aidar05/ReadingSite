f86a306439dbc02a10c156bfde958d0e862352ca
Aidar <sharipovaydar200515@gmail.com>
2024-02-17 03:59:04 +0600

    Удалил ненужную часть

--- VueMigration/course_work/src/components/about_component/About.vue
+++ VueMigration/course_work/src/components/about_component/About.vue
@@ -6,7 +6,6 @@
 
       <Description/>
 
-      <Best_writers/>
     </div>
   </div>
 </template>
@@ -15,12 +14,11 @@
 import "../../assets/css/about/about.css"
 import Attributes from "./Attributes.vue";
 import Description from "./Description.vue";
-import Best_writers from "./Best_writers.vue"
 
 export default {
     name: 'About',
     components: { 
-      Attributes, Description, Best_writers
+      Attributes, Description
     }
 }
 
